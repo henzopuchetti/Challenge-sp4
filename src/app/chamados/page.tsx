2cfa3b2b1b0ce7a242a950b7a5b1a5ec2e4ad3c0
henzo <84385799+henzopuchetti@users.noreply.github.com>
2024-11-04 20:00:51 -0300

    Update page.tsx

--- src/app/chamados/page.tsx
+++ src/app/chamados/page.tsx
@@ -1,3 +1,4 @@
+//página chamados
 import Navbar from "../components/Navbar/Navbar";
 import FormChamados from "../components/FormChamados/FormChamados";
 import Footer from "../components/Footer/Footer";
